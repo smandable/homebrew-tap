adef9df708b97f48f2d039ef11173758851eed2f
Sean <smandable@gmail.com>
2026-05-06 12:09:21 -0400

    Update saddle to 1.2.1

--- Casks/saddle.rb
+++ Casks/saddle.rb
@@ -1,6 +1,6 @@
 cask "saddle" do
-  version "1.2"
-  sha256 "aebc612cb791c46a07eee98947587282e4ba3a0d36020ed69cc4865f7cfc4267"
+  version "1.2.1"
+  sha256 "7a4008190032529b9f89e2a54afa5ec8f15fc2940b9442525c2197f3f369e0bf"
 
   url "https://github.com/smandable/Saddle/releases/download/v#{version}/Saddle-#{version}.dmg"
   name "Saddle"
