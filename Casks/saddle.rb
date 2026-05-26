8f8e71e17b48ee201ee9f8eb8b53550b61fbf790
Sean <smandable@gmail.com>
2026-05-26 14:34:05 -0400

    saddle 1.4

--- Casks/saddle.rb
+++ Casks/saddle.rb
@@ -1,6 +1,6 @@
 cask "saddle" do
-  version "1.3"
-  sha256 "6bf9c94912026a81c80754da082a59c6994fde5eab4964ca898ef55dfc702843"
+  version "1.4"
+  sha256 "dd39229297bc7ede7aa2ec3d4b00823d717699361da74234641b70b7f03edc9f"
 
   url "https://github.com/smandable/Saddle/releases/download/v#{version}/Saddle-#{version}.dmg"
   name "Saddle"
