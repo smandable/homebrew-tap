e2b21eaf10a32e5396aeb2d91715eeb0089860c6
Sean <smandable@gmail.com>
2026-05-26 14:39:25 -0400

    saddle 1.4: rebuild DMG with Applications symlink

--- Casks/saddle.rb
+++ Casks/saddle.rb
@@ -1,6 +1,6 @@
 cask "saddle" do
   version "1.4"
-  sha256 "dd39229297bc7ede7aa2ec3d4b00823d717699361da74234641b70b7f03edc9f"
+  sha256 "55eead7c5f1cfe25837fbb17fe3eccedd59187339415f15fe02b648314578d59"
 
   url "https://github.com/smandable/Saddle/releases/download/v#{version}/Saddle-#{version}.dmg"
   name "Saddle"
